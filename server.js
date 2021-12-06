de0c388b85fe4505ad3922fc1191ab95f6aca322
Conner Matson <conner_matson@science.edu>
2021-12-06 12:53:42 -0600

    add mongoDB

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 // importing the dependencies
-const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const port = process.env.PORT || 4000;
+const express = require("express");
+const bodyParser = require("body-parser");
+// const { MongoClient } = require("mongodb");
 
-const app = express();
-// adding Helmet to enhance your API's security
+// const url = `mongodb://conner:${process.env.MONGO_DB_PASSWORD}@acellus-plus-db-dev.z2tdi.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`;
+// MongoClient.connect(url, (err, client) => {
+// 	if (err) console.log(err);
+// 	const db = client.db("acellus-plus-db-dev");
+// 	var data = db.collection("courses").find({});
+// 	console.log(data ? data : "no data dummy");
+// });
+
+// adding Helmet to enhance security
 // using bodyParser to parse JSON bodies into JS objects
 // enabling CORS for all requests
 // adding morgan to log HTTP requests
+const app = express();
+app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json());
-app.use(cors());
 app.use(morgan("@:remote-addr | :method | :url | :status | in :total-time ms | :user-agent"));
 
 // importing arrays to act as db -temporary solution
@@ -37,10 +45,9 @@ app.get("/api/resources", (req, res) => {
 app.get("/api/textbook", (req, res) => {
 	res.send(textbook);
 });
-
 /****** API ROUTES  ******/
 
 // starting the server
-app.listen(port, () => {
-	console.log(`listening on port ${port}`);
+app.listen(process.env.PORT || 4000, () => {
+	console.log(`listening at [ http://localhost:${process.env.PORT || 4000} ]`);
 });
